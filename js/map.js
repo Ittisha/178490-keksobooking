78979a13427bd3f95e71a1a201e872840606dd4d
Ann Zezulkina <annzez@gmail.com>
2017-08-28 09:54:58 +0300

    add form fields binding, form validation and submit

--- js/map.js
+++ js/map.js
@@ -313,7 +313,7 @@ var getOfferIndex = function (currentSrc) {
  * @param {Node} currentPinImage
  */
 var showDialog = function (currentPinImage) {
-  if ((currentPinImage.className === 'rounded') && (currentPinImage.parentNode.className !== 'pin  pin__main')) {
+  if (currentPinImage.className === 'rounded' && !currentPinImage.parentNode.classList.contains('pin__main')) {
     var index = getOfferIndex(currentPinImage.getAttribute('src'));
     var offer = createLodgeCard(lodgeTemplateContent, offersList[index]);
 
@@ -394,12 +394,27 @@ document.addEventListener('keydown', onDialogEscPress);
 var noticeForm = document.querySelector('.notice__form');
 var timeInField = noticeForm.querySelector('#timein');
 var timeOutField = noticeForm.querySelector('#timeout');
-var roomNumberField = noticeForm.querySelector('#room_number');
-var capacityField = noticeForm.querySelector('#capacity');
-var submitButton = noticeForm.querySelector('.form__submit');
+var roomsNumberField = noticeForm.querySelector('#room_number');
+var guestsField = noticeForm.querySelector('#capacity');
 var titleField = noticeForm.querySelector('#title');
 var addressField = noticeForm.querySelector('#address');
+var lodgeTypeField = noticeForm.querySelector('#type');
+var priceField = noticeForm.querySelector('#price');
+var submitButton = noticeForm.querySelector('.form__submit');
 
+var TYPES_MIN_PRICES = {
+  bungalo: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000
+};
+var HUNDRED_ROOMS_VALUE = '100';
+var NO_GUESTS = '0';
+
+/**
+ * Rewrite title validation messages in russian
+ * @param {Node} inputNode
+ */
 var rewriteTitleValidationMessages = function (inputNode) {
   var minLength = inputNode.getAttribute('minlength');
   var maxLength = inputNode.getAttribute('maxlength');
@@ -414,7 +429,10 @@ var rewriteTitleValidationMessages = function (inputNode) {
     inputNode.setCustomValidity('');
   }
 };
-
+/**
+ * Rewrite address validation messages in russian
+ * @param {Node} inputNode
+ */
 var rewriteAddressValidationMessages = function (inputNode) {
   if (inputNode.validity.valueMissing) {
     inputNode.setCustomValidity('Обязательное поле');
@@ -422,20 +440,10 @@ var rewriteAddressValidationMessages = function (inputNode) {
     inputNode.setCustomValidity('');
   }
 };
-
-var setInputCustomValidity = function (inputNode, callback) {
-  if (!inputNode.validity.valid) {
-    callback(inputNode);
-  }
-};
-
-var onTitleFieldInvalid = function () {
-  setInputCustomValidity(titleField, rewriteTitleValidationMessages);
-};
-var onAddressFieldInvalid = function () {
-  setInputCustomValidity(addressField, rewriteAddressValidationMessages);
-};
-
+/**
+ * Validate title min-length for Edge
+ * @param {Object} evt
+ */
 var onTitleFieldInput = function (evt) {
   var minLength = evt.target.getAttribute('minlength');
   var target = evt.target;
@@ -445,36 +453,10 @@ var onTitleFieldInput = function (evt) {
     target.setCustomValidity('');
   }
 };
-
-titleField.addEventListener('input', onTitleFieldInput);
-titleField.addEventListener('invalid', onTitleFieldInvalid);
-addressField.addEventListener('input', onAddressFieldInvalid);
-addressField.addEventListener('invalid', onAddressFieldInvalid);
-
-var onTimeInTimeOutChange = function (evt) {
-  var anotherTimeField = evt.target === timeInField ? timeOutField : timeInField;
-  anotherTimeField.value = evt.target.value;
-};
-
-timeInField.addEventListener('change', onTimeInTimeOutChange);
-timeOutField.addEventListener('change', onTimeInTimeOutChange);
-
-var lodgeTypeField = noticeForm.querySelector('#type');
-var priceField = noticeForm.querySelector('#price');
-
-var typesMinPrices = {
-  bungalo: 0,
-  flat: 1000,
-  house: 5000,
-  palace: 10000
-};
-
-var onLodgeTypeFieldChange = function (evt) {
-  var minPrice = typesMinPrices[evt.target.value];
-  priceField.setAttribute('min', minPrice);
-  priceField.value = minPrice;
-};
-
+/**
+ * Rewrite price validation messages in russian
+ * @param {Node} inputNode
+ */
 var rewritePriceValidationMessage = function (inputNode) {
   var minPrice = inputNode.getAttribute('min');
   var maxPrice = inputNode.getAttribute('max');
@@ -489,33 +471,114 @@ var rewritePriceValidationMessage = function (inputNode) {
     inputNode.setCustomValidity('');
   }
 };
+/**
+ * Call callback-function if input invalid
+ * @param {Node} inputNode
+ * @param {Function} callback
+ */
+var setInputCustomValidity = function (inputNode, callback) {
+  if (!inputNode.validity.valid) {
+    callback(inputNode);
+  }
+};
+/**
+ * Call new validation messages if title field invalid
+ */
+var onTitleFieldInvalid = function () {
+  setInputCustomValidity(titleField, rewriteTitleValidationMessages);
+};
+/**
+ * Call new validation messages if address field invalid
+ */
+var onAddressFieldInvalid = function () {
+  setInputCustomValidity(addressField, rewriteAddressValidationMessages);
+};
+/**
+ * Call new validation messages on address field input
+ */
+var onAddressFieldInput = function () {
+  setInputCustomValidity(addressField, rewriteAddressValidationMessages);
+};
+/**
+ * Call new validation messages if price field invalid
+ */
 var onPriceFieldInvalid = function () {
   setInputCustomValidity(priceField, rewritePriceValidationMessage);
 };
-priceField.addEventListener('invalid', onPriceFieldInvalid);
-lodgeTypeField.addEventListener('change', onLodgeTypeFieldChange);
-
-var onRoomNumberChange = function (evt) {
-  Array.prototype.forEach.call(capacityField.options, function (elem) {
-    elem.disabled = false;
-    if (evt.target.value === '100') {
-      if (elem.value !== '0') {
-        elem.disabled = true;
-      }
-    } else {
-      if (elem.value === '0') {
-        elem.disabled = true;
-      }
-      if (elem.value > roomNumberField.value) {
-        elem.disabled = true;
-      }
-    }
-  });
-  capacityField.value = roomNumberField.value === '100' ? '0' : '1';
+/**
+ * Call new validation messages on price field input
+ */
+var onPriceFieldInput = function () {
+  setInputCustomValidity(priceField, rewritePriceValidationMessage);
 };
 
-roomNumberField.addEventListener('change', onRoomNumberChange);
+titleField.addEventListener('input', onTitleFieldInput);
+titleField.addEventListener('invalid', onTitleFieldInvalid);
 
+addressField.addEventListener('input', onAddressFieldInput);
+addressField.addEventListener('invalid', onAddressFieldInvalid);
+
+priceField.addEventListener('input', onPriceFieldInput);
+priceField.addEventListener('invalid', onPriceFieldInvalid);
+
+/**
+ * Bind time in and time out changes
+ * @param {Object} evt
+ */
+var onTimeInTimeOutChange = function (evt) {
+  var anotherTimeField = evt.target === timeInField ? timeOutField : timeInField;
+  anotherTimeField.value = evt.target.value;
+};
+
+timeInField.addEventListener('change', onTimeInTimeOutChange);
+timeOutField.addEventListener('change', onTimeInTimeOutChange);
+
+/**
+ * Bind lodge type and min price
+ * @param {Object} evt
+ */
+var onLodgeTypeFieldChange = function (evt) {
+  var minPrice = TYPES_MIN_PRICES[evt.target.value];
+  priceField.setAttribute('min', minPrice);
+  priceField.value = minPrice;
+};
+
+lodgeTypeField.addEventListener('change', onLodgeTypeFieldChange);
+
+/**
+ * Bind rooms and guests quantities
+ * @param {Object} evt
+ */
+var onRoomsGuestsChange = function (evt) {
+  var anotherField = evt.target === roomsNumberField ? guestsField : roomsNumberField;
+  var switchedValue = evt.target.value;
+
+  if (anotherField === guestsField) {
+    if (switchedValue === HUNDRED_ROOMS_VALUE) {
+      anotherField.value = NO_GUESTS;
+    } else if (+switchedValue > +anotherField.value) {
+      anotherField.value = anotherField.value === '0' ? switchedValue : anotherField.value;
+    } else {
+      anotherField.value = switchedValue;
+    }
+  } else {
+    if (switchedValue === '0') {
+      anotherField.value = '100';
+    } else if (+switchedValue > +anotherField.value || (+switchedValue < +anotherField.value && anotherField.value === '100')) {
+      anotherField.value = switchedValue;
+    } else {
+      anotherField.value = anotherField.value;
+    }
+  }
+};
+
+roomsNumberField.addEventListener('change', onRoomsGuestsChange);
+guestsField.addEventListener('change', onRoomsGuestsChange);
+
+/**
+ * Check form validation
+ * @return {boolean}
+ */
 var checkValidationSubmit = function () {
   var result = true;
   Array.prototype.forEach.call(noticeForm.elements, function (elem) {
@@ -533,17 +596,36 @@ var checkValidationSubmit = function () {
   return result;
 };
 
+/**
+ * Send form if validation passed, reset data after submit
+ * @param {Node} form
+ */
 var sendForm = function (form) {
-  form.submit();
-  form.reset();
+  if (checkValidationSubmit()) {
+    form.submit();
+    form.reset();
+  }
 };
+/**
+ * Send form on submit button click
+ * @param {Object} evt
+ */
 var onSubmitButtonClick = function (evt) {
   evt.preventDefault();
-  if (checkValidationSubmit()) {
+  sendForm(noticeForm);
+};
+/**
+ * Send form on submit button Enter press
+ * @param {Object} evt
+ */
+var onSubmitButtonEnterPress = function (evt) {
+  if (evt.keyCode === KEY_CODES.enter) {
+    evt.preventDefault();
     sendForm(noticeForm);
   }
 };
 
 submitButton.addEventListener('click', onSubmitButtonClick);
+submitButton.addEventListener('keydown', onSubmitButtonEnterPress);
 
 
